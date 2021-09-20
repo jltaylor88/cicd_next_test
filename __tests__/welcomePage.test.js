92c1c69c2cb6d6ff4a8b6da05c55bbe624339ed6
jtaylor <jason.taylor@6bdigital.com>
2021-09-20 12:50:12 +0100

    Include additional test and own component to test importr export

--- __tests__/welcomePage.test.js
+++ __tests__/welcomePage.test.js
@@ -1,7 +1,14 @@
 import { render, screen } from "@testing-library/react";
+import CustomHeader from "../components/CustomHeader";
 import Home from "../pages/index";
 
 it("should render the NextJS welcome title", () => {
   render(<Home />);
   const title = screen.getByRole("heading", { name: "Welcome to Next.js!" });
+});
+
+it("shoudl render the custom header", () => {
+  render(<CustomHeader />);
+  const customHeader = screen.getByText(/I am a custom header/i);
+  expect(customHeader).toBeTruthy();
 });
---
+++ components/CustomHeader/index.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+const CustomHeader : FC = () => {
+    return <div>I am a custom header</div>
+}
+
+export default CustomHeader;
