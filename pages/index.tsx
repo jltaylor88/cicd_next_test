49fb9714114ec27c0504bef4988bd4c437c8c760
jtaylor <jason.taylor@6bdigital.com>
2021-09-20 13:47:50 +0100

    revert test fix to break test action

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Next.jsss!</a>
         </h1>
         <CustomHeader />
 
