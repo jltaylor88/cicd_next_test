ec28f070bc78c7de2c73043f101d8dfa60d48414
jtaylor <jason.taylor@6bdigital.com>
2021-09-21 08:51:24 +0100

    Add build step in workflow

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,6 @@ import CustomHeader from '../components/CustomHeader'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
- --
   return (
     <div className={styles.container}>
       <Head>
